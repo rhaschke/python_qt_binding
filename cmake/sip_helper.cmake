b55d6c3eb789a103d3026c50262fd05efbfcfde6
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2020-05-13 14:52:44 +0200

    QUICK FIX: silence deprecated-copy warnings for sip build

--- cmake/sip_helper.cmake
+++ cmake/sip_helper.cmake
@@ -87,6 +87,7 @@ function(build_sip_binding PROJECT_NAME SIP_FILE)
     add_custom_command(
         OUTPUT ${SIP_BUILD_DIR}/Makefile
         COMMAND ${PYTHON_EXECUTABLE} ${sip_SIP_CONFIGURE} ${SIP_BUILD_DIR} ${SIP_FILE} ${sip_LIBRARY_DIR} \"${INCLUDE_DIRS}\" \"${LIBRARIES}\" \"${LIBRARY_DIRS}\" \"${LDFLAGS_OTHER}\"
+        COMMAND sed -i 's/ -Wall / -Wall -Wno-deprecated-copy /g' ${SIP_BUILD_DIR}/Makefile
         DEPENDS ${sip_SIP_CONFIGURE} ${SIP_FILE} ${sip_DEPENDS}
         WORKING_DIRECTORY ${sip_SOURCE_DIR}
         COMMENT "Running SIP generator for ${PROJECT_NAME} Python bindings..."
